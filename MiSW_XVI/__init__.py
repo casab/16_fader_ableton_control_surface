e2e2b409598b94c51246a3780a550918372fd021
Engin Al <enginal@gmail.com>
2023-10-04 20:40:07 +0300

    Added two scripts to control 16 track volumes

---
+++ MiSW_XVI/__init__.py
@@ -0,0 +1,11 @@
+
+import Live
+from .MiSW_XVI import MiSW_XVI
+
+def create_instance(c_instance):
+    ' Creates and returns the APC20 script '
+    return MiSW_XVI(c_instance)
+
+
+# local variables:
+# tab-width: 4
---
+++ MiSW_XVI2/__init__.py
@@ -0,0 +1,11 @@
+
+import Live
+from .MiSW_XVI2 import MiSW_XVI2
+
+def create_instance(c_instance):
+    ' Creates and returns the APC20 script '
+    return MiSW_XVI2(c_instance)
+
+
+# local variables:
+# tab-width: 4
